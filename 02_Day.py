7612b6b5717c5fdcd45f19871cadddd9dae27de2
Muhammad Anas <129672108+MuhammadAnasAI@users.noreply.github.com>
2025-07-21 07:59:31 +0500

    Python Day 2

---
+++ 02_Day.py
@@ -0,0 +1,27 @@
+# Indentation in Python
+a = 4
+b = 5
+c = 6
+print(a > b)
+if a > c:
+    print('a is greater than c')
+else:
+    print('c is greater than a')
+
+# user input progam in python
+#name = input('What is your name?')
+#print('Hello' , name, 'Khan')    
+
+#person_A = input('Name of first person')
+#person_B = input('Name of second person')
+#age_A = input('Age of first person')
+#age_B = input('Age of second person')
+#if age_A > age_B:
+ #   print(person_A, 'is older than', person_B)
+#else :
+#    print(person_B, 'is older than', person_A)    
+
+
+list = ['apple', 'banana', 'mango', 'grapes', 'milk']
+print(list[3]) 
+print(list[-1])   
