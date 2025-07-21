79535e0c9b3771047a43e382e631411ec6954631
Muhammad Anas <129672108+MuhammadAnasAI@users.noreply.github.com>
2025-07-21 08:05:24 +0500

    Python Day 1

---
+++ 01_Day.py
@@ -0,0 +1,51 @@
+# String
+print('Anas')
+print("Muhammad Anas")
+print(''' I am Pakistani''')
+# Operators
+print(5 + 8)
+print(7.5 - 5.5)
+a = [2, 3, 4]
+b = [5, 6, 7]
+c = [5, 6, 7]
+print(a + b)
+print(a == b) # Boolean
+print(b == c)
+# Variables 
+a = 4
+b = 'Anas Khan'
+c = 6.7
+print(a)
+print(b)
+# Operators in Python
+x = 3
+y = 6
+z = 4.5
+a = 'Pakistan'
+b = 'my Country'
+print(x + y)
+print(a + b)
+print(y - x)
+print(x*y)
+print(x**y)
+print(y-z)
+print(y/x)
+print(x+y-z/x*5)
+x = 12
+y = 6
+z = 7.6
+print(x-y)
+print(x)
+# Comparision Operators
+print(x > y)
+print(x < y)
+print(x == z)
+print(y != x)
+# Data Types 
+i = ('Pakistan', 'Islamabad', 'Karachi') # tuple
+r = (1, 2, 3)
+p = ['Kashmir', 'Faislabad', 'Lahore'] # List
+k = [1, 2, 3, 5, 6] 
+q = {"Lahore": 1, "Islamabad": 2, "Karachi": 3} # Dictionary
+t = 4.5 # float
+print(q)
